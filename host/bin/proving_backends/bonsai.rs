c5dac8830b2a226eabb345b53b5eea50ea04d71a
Willem Olding <willemolding@gmail.com>
2025-04-04 19:21:01 +1300

    update readme and env example

--- host/bin/proving_backends/bonsai.rs
+++ host/bin/proving_backends/bonsai.rs
@@ -1,19 +1,15 @@
 use std::time::Duration;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use bonsai_sdk::non_blocking::Client;
 use log::info;
 use r0vm_helios_methods::R0VM_HELIOS_GUEST_ELF;
 use risc0_zkvm::compute_image_id;
 use risc0_zkvm::Receipt;
 
-pub async fn get_proof(input: Vec<u8>) -> Result<Receipt> {
-    if !(std::env::var("BONSAI_API_URL").is_ok() && std::env::var("BONSAI_API_KEY").is_ok()) {
-        return Err(anyhow::anyhow!(
-            "BONSAI_API_URL and BONSAI_API_KEY env vars must be set"
-        ));
-    }
+const POLLING_INTERVAL: Duration = Duration::from_secs(5);
 
+pub async fn get_proof(input: Vec<u8>) -> Result<Receipt> {
     let client = Client::from_env("2.0.0")?;
 
     // Compute the image_id, then upload the ELF with the image_id as its key.
@@ -38,14 +34,38 @@ pub async fn get_proof(input: Vec<u8>) -> Result<Receipt> {
                 res.status,
                 res.state.unwrap_or_default()
             );
-            std::thread::sleep(Duration::from_secs(5));
+            std::thread::sleep(POLLING_INTERVAL);
             continue;
         }
         if res.status == "SUCCEEDED" {
-            // Download the receipt, containing the output
-            let receipt_url = res
-                .receipt_url
-                .expect("API error, missing receipt on completed session");
+            // Request that Bonsai compress further, to Groth16.
+            let snark_session = client.create_snark(session.uuid).await?;
+            let snark_receipt_url = loop {
+                let res = snark_session.status(&client).await?;
+                match res.status.as_str() {
+                    "RUNNING" => {
+                        std::thread::sleep(POLLING_INTERVAL);
+                        continue;
+                    }
+                    "SUCCEEDED" => {
+                        break res.output.with_context(|| {
+                            format!(
+                            "Bonsai prover workflow [{}] reported success, but provided no receipt",
+                            snark_session.uuid
+                        )
+                        })?;
+                    }
+                    _ => {
+                        anyhow::bail!(
+                            "Bonsai prover workflow [{}] exited: {} err: {}",
+                            snark_session.uuid,
+                            res.status,
+                            res.error_msg
+                                .unwrap_or("Bonsai workflow missing error_msg".into()),
+                        );
+                    }
+                }
+            };
 
             let stats = res
                 .stats
@@ -55,7 +75,7 @@ pub async fn get_proof(input: Vec<u8>) -> Result<Receipt> {
                 stats.cycles, stats.total_cycles
             );
 
-            let receipt_buf = client.download(&receipt_url).await?;
+            let receipt_buf = client.download(&snark_receipt_url).await?;
             let receipt: Receipt = bincode::deserialize(&receipt_buf)?;
             return Ok(receipt);
         } else {
